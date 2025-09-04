0917fd4c24a46393ea7cc0fd9f280b3dc7be5c8b
nftpro1212 <fozilkhalimov95@gmail.com>
2025-09-05 01:22:25 +0500

    Delete backend directory

--- backend/src/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  User: require('./user'),
-  NFT: require('./nft'),
-  Transaction: require('./transaction'),
-  UserNft: require('./userNft'),
-};
--- backend/src/utils/verifyWebhook.js
+++
@@ -1,7 +0,0 @@
-// Placeholder: If you integrate a payment gateway webhook (e.g. Cryptomus/NowPayments),
-// verify signature here. For pure on-chain TON verify, you may not need this.
-module.exports = function verifyWebhook(req) {
-  // e.g., const signature = req.headers['x-nowpayments-sig'];
-  // return validate(signature, req.body, process.env.WEBHOOK_SECRET);
-  return true;
-};
