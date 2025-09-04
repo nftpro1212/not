da9895cde59ed6d3787b15d225cfb54e4a14f6ad
nftpro1212 <fozilkhalimov95@gmail.com>
2025-09-05 01:02:32 +0500

    Delete frontend directory

--- frontend/src/telegram/telegram.js
+++
@@ -1,9 +0,0 @@
-export default function telegramAuth() {
-  // Hozircha test uchun mock
-  return {
-    id: "123456",
-    username: "cyberpunk_user",
-    first_name: "Neo",
-    last_name: "Matrix"
-  };
-}
